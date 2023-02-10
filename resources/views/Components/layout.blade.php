2be74f113066078e43e4ddc1760e35671315c2ee
Andrea Alemanno <andrea.alemanno91@icloud.com>
2023-02-10 13:00:25 +0100

    Initial commit

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    <x-navbar />
+    <x-navbar2 />
+    <x-masthead />
+
+
+<div class="min-vh-100">
+    {{ $slot }}
+</div>
+
+
+
+    <x-footer /> 
+</body>
+</html>
---
+++ resources/views/Components/masthead.blade.php
@@ -0,0 +1,18 @@
+<header class="masthead">
+  <div class="container-fluid h-100 w-100">
+    <div class="row h-100 align-items-center justify-content-right">
+      <div class="col-12 col-md-6">
+      </div>
+      <div class="col-12 col-md-6 text-center">
+        <h1 class="fw-bold">Presto.it</h1>
+        <p class="fw-bold">I migliori prodotti a portata di click</p>
+        <div class="">
+            <button class="button-33 me-5">Vendi</button>
+            <button class="button-33">Compra</button>
+    </div>
+      </div>
+    </div>
+  </div>
+  
+  
+</header>
