985ce535803c41bac63b09b97f19162ddc5273c7
Gianluigi Spina <spina.gianluigi@hotmail.it>
2023-02-21 16:57:06 +0100

    Finito responsive navbar, categorie card

--- resources/views/Components/card-product.blade.php
+++ resources/views/Components/card-product.blade.php
@@ -2,7 +2,7 @@
   <img src="{{!$product->images()->get()->isEmpty() ? $product->images()->first()->getUrl(300,300) : 'https://picsum.photos/200/200'}}" class=" myimg card-img-top " alt="{{$product->title}}">
   <div class="card-body">
     <h5 class="card-title text-dark ">{{$product->title}}</h5>
-    <b><p class="card-text">{{ __('ui.categoriesCard') }} </b><a href="{{route('showCategory', ['category' => $product->category])}}" class="text-decoration-none textprova">{{$product->category->name}}</a></p>
+    <a href="{{route('showCategory', ['category' => $product->category])}}" class="text-decoration-none textprova">{{$product->category->name}}</a></p>
     <p class="card-text"><i class="fa-solid fa-euro-sign"></i> {{$product->price}}</p>
     <p class="card-text"><i class="fa-solid fa-calendar-days"></i> {{$product->created_at->format('d/m/Y')}}</p>
     <center>
--- resources/views/Components/navbar.blade.php
+++ resources/views/Components/navbar.blade.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <a class="navbar-brand logo" href="{{ route('home') }}"><img src="\img\logo2.png" class="logo" alt="logo2"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+     <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto justify-content-center my-search">
@@ -11,10 +11,10 @@
           <button class="my-button btn" type="submit">{{ __('ui.search') }}</button>
         </form>
       </ul>
-      <ul class="navbar-nav ms-auto">
-        <div class="dropdown ">
-          <button class=" dropdown-title btn dropdown-toggle" type="button" id="languageDropdown" data-bs-toggle="dropdown" aria-expanded="false">{{__('ui.chooseLang')}}</button>
-          <ul class="dropdown-menu" aria-labelledby="languageDropdown">
+      <ul class="navbar-nav navbar-custom1">
+        <div class="dropdown dropdown-custom ">
+          <button class=" dropdown-title btn dropdown-toggle " type="button" id="languageDropdown" data-bs-toggle="dropdown" aria-expanded="false">{{__('ui.chooseLang')}}</button>
+          <ul class="dropdown-menu " aria-labelledby="languageDropdown">
             <li class="nav-link "><x-_locale lang="it" />Italiano</li>
             <li class="nav-link"><x-_locale lang="en" />English</li>
             <li class="nav-link"><x-_locale lang="es" />Español</li>
@@ -25,11 +25,11 @@
             <a class=" benvenuto nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {{__('ui.welcome')}} {{ Auth::user()->name }}
             </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <ul class="dropdown-menu " aria-labelledby="navbarDropdown">
               @if(Auth::user()->is_revisor)
                 <li>
                   <a class="dropdown-item1 text-light" href="{{ route('revisor.index') }}">{{ __('ui.revisorZone') }}
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"> {{ App\Models\Product::toBeRevisionedCount() }}
+                    <span class="position-absolute pallino top-0 start-100 translate-middle badge rounded-pill bg-danger"> {{ App\Models\Product::toBeRevisionedCount() }}
                       <span class="visually-hidden">{{ __('ui.unread messages') }}</span>
                     </span>
                   </a>
